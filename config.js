55daf13e26d0f5fbb9f0d840738749556032746b
amritatwal <amritatwal04@gmail.com>
2022-07-06 12:53:29 +0100

    Removed console.log from config.js

--- config.js
+++ config.js
@@ -1,5 +1,4 @@
 // The process object is within the global object in Node and I use it to access the relevant API url through the object properties.
 const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
 
-console.log(API_KEY);
 export default API_KEY;
