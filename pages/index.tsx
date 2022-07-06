b13ba513e2d7fa672a5839b881719db566d9d731
amritatwal <amritatwal04@gmail.com>
2022-07-06 12:39:02 +0100

    Fetch API set-up to call Ambee once input state has changed

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-import Results from '../components/results/results';
-import SearchBar from '../components/searchBar/searchBar';
+import React, { useState, useEffect } from 'react';
+import Results from '../src/components/results/results';
+import SearchBar from '../src/components/searchBar/searchBar';
 
-export default function Home({ data }) {
+export default function Home() {
   const [input, setInput] = useState<string>();
+  const [postcode, setPostcode] = useState<string>();
+  const [country, setCountry] = useState<string>();
 
   function handleChange(event: React.KeyboardEvent<HTMLInputElement>) {
     if (event.key === "Enter") {
@@ -12,8 +14,23 @@ export default function Home({ data }) {
     }
   }
 
-  return (
-      <SearchBar handleChange={handleChange}/>
-    )
-}
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(`https://api.ambeedata.com/latest/by-city?city=${input}`, {
+        "method": "GET",
+        "headers": {
+          "x-api-key": "bea4eac02dcf0f3b8f6c1e7d1e8b261775a89e3f49e76938b60c037fed0bba13",
+          "Content-type": "application/json"
+        }
+      });
+      const data = await res.json()
+      console.log(data.stations[0])
+    }
+    fetchData() 
+  }, [input]);
 
+
+  return (
+    <SearchBar handleChange={handleChange}/>
+  )
+}
