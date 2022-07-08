c94d0385996948b839209ddb2f5f094200321243
amritatwal <amritatwal04@gmail.com>
2022-07-08 20:00:26 +0100

    Added svg icons for conditional rendering in overview section in feedback

---
+++ libs/data/icons.js
@@ -0,0 +1,18 @@
+const icons = [
+  {
+    category: "good",
+    src: "https://svgshare.com/i/ixW.svg",
+  },
+  {
+    category: "moderate",
+    src: "https://svgshare.com/i/iy1.svg",
+  },
+  {
+    category: "poor",
+    src: "https://svgur.com/s/iy2",
+  },
+  {
+    category: "unhealthy",
+    src: "https://svgur.com/s/iw9",
+  },
+];
